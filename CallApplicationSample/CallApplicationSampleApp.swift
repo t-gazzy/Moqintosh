987a849b6687f12feda1d695797c0461152d32a1
t-gazzy <t.kaji@ntt.com>
2026-04-22 01:35:37 +0900

    feat: call application

---
+++ CallApplicationSample/CallApplicationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallApplicationSampleApp.swift
+//  CallApplicationSample
+//
+//  Created by Takemasa Kaji on 2026/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CallApplicationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CallApplicationSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CallApplicationSample
+//
+//  Created by Takemasa Kaji on 2026/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
